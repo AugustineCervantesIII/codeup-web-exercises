0df623fadc8594ae70e4f4bc2fa31551abd69007
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-04-28 09:03:25 -0500

    adding loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,24 @@
-function showMultiplicationTable(){
-
+'use strict';
+// function showMultiplicationTable() {
+//
+// }
+for(let counter = 7; counter <= 10; counter++){
+    console.log(counter);
 }
-console.log(showMultiplicationTable())
+
+for (let counter = 3; counter >= 0; --counter){
+    console.log(counter);
+}
+
+
+for (var i = 7; i <= 70; i++) {
+
+    if (i % 7 * 2) {
+        // number isn't even
+        // odd numbers aren't as cool
+        // skip the rest of the loop and continue with the next iteration
+        continue;
+    }
+
+    console.log('7 x 1 = ' + i);
+}
