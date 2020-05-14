bba5824246277c0ebe73f98567b342bb639adcd0
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-05-14 16:12:12 -0500

    adding jquery attribute lecture and axercise

---
+++ js/essential-methods-lec.js
@@ -0,0 +1,126 @@
+"use strict";
+
+$(document).ready(function() {
+
+    //name input goes through random sorting here
+    $('#sort-wizard').click(function(e){
+        let house = Math.floor(Math.random() * 4);
+        let newWizard = $('#new-wizard');
+        let newMember = $('<li></li>').text(newWizard.val());
+        switch(house){
+            case 0:
+                $('#gryffindor').append(newMember);
+                break;
+            case 1:
+                $('#slytherin').append(newMember);
+                break;
+            case 2:
+                $('#ravenclaw').append(newMember);
+                break;
+            default:
+                $('#hufflepuff').append(newMember);
+                break;
+        }
+        newWizard.val('');
+    });
+
+    /**********************************************
+     * 			Attributes and CSS
+     *********************************************/
+
+    // TODO TOGETHER:  Using an event listener and css method, increase the font-size of 'Sorting Hat' when clicked.
+    //    How can font size be changed using .html()?
+    $('#main-heading').click(function(){
+        // $(this).css('font-size', '4em');
+        $(this).html('<h4><em>My Sorting Hat</em></h4>');
+    });
+    // how can the font size be changed with .html()
+
+    $('#gryffindor-house').click(function(){
+        $(this).css('color', 'red');
+    });
+
+    $('#slytherin-house').click(function(){
+       $(this).css('color', 'green');
+    });
+
+    // TODO TOGETHER: store the inner text of #main-heading in a variable
+    var mainHeading = $('#main-heading').text();
+    alert(mainHeading);
+
+    // TODO: Using a css method, change the banner background-color to black
+    //  Hint: make sure you use the right selector
+
+    $('.banner').css('background-color', '#000');
+
+
+    // TODO TOGETHER: When the 'Houses' button is clicked, add the class 'house' to anything with a class of 'house-name'
+    // Click on button with this id
+    $('#highlight-houses').click(function(){
+        // target all elements with class of .house-name
+        // add class 'house' to its html
+        // $('.house-name').addClass('house font-size');
+        // $('.house-name').removeClass('house');
+        $('.house-name').toggleClass('house');
+    });
+
+    // TODO: Comment out the code above. Add the class of 'house' to all the 'house-names'
+    //  Write the event listener to remove the class instead
+
+    //  TODO TOGETHER: Refactor your addClass and removeClass methods to toggle the 'house' class instead
+
+    $('#lock-g').click(function(){
+        $('#gryffindor-house').toggleClass('gryffindor');
+        if($('#gryffindor-house').hasClass('gryffindor')){
+            // this refers to the element that triggers the event listener
+            $(this).text('unlock');
+        }else{
+            $(this).text('lock in');
+        }
+    });
+
+    /**********************************************
+     * 			       Traversing
+     *********************************************/
+    $('#gryffindor').children();
+
+    console.log($('#josh').next());
+
+    // TODO TOGETHER: console log each list element's text
+
+    let numbers = [1, 2, 3, 4, 5];
+    numbers.forEach(function(number){
+        console.log(number);
+    });
+
+    console.log($('li'));
+
+    $('li').each(function(){
+        console.log($(this).html());
+    });
+
+    // TODO TOGETHER: select all list elements and console log the first match
+    console.log($('li').first().html());
+
+    // TODO TOGETHER: select all list elements and console log the last match
+    console.log($('li').last().html());
+
+    // TODO TOGETHER: When I click on a list element, highlight its parent
+    //this highlights the ul for any list items that are clicked
+    $('li').click(function(){
+        //method chaining
+        $(this).parent().css('background-color', 'lightgreen');
+    });
+
+    // TODO TOGETHER: When I click into a <ul>, console log last child in that group
+    $('ul').click(function(){
+        console.log($(this).children().last().html());
+    });
+
+    // TODO TOGETHER: When I click on any list element, console log the next element
+    $('#r-heading').click(function(){
+       $(this).next().css('background-color', 'blue');
+       //$(this).next().addClass('ravenclaw');
+    });
+
+});
